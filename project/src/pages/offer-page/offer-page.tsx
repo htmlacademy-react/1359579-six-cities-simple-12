8a3a59e5ac8fc8a597a2646aa436154fd088b4f0
alenushka-jpg <alionxlo@gmail.com>
2023-03-22 21:11:32 +0200

    Передает моки review компоненту offer-page

--- project/src/pages/offer-page/offer-page.tsx
+++ project/src/pages/offer-page/offer-page.tsx
@@ -4,6 +4,7 @@ import {Helmet} from 'react-helmet-async';
 import { Offers } from '../../types/offer';
 import { Reviews } from '../../types/review';
 import { useParams } from 'react-router-dom';
+import ReviewsItem from '../../components/reviews-item/reviews-tem';
 
 type OfferPageProps = {
   offers: Offers;
@@ -132,28 +133,10 @@ function OfferPage({ offers, reviews}: OfferPageProps): JSX.Element {
               <section className="property__reviews reviews">
                 <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
                 <ul className="reviews__list">
-                  <li className="reviews__item">
-                    <div className="reviews__user user">
-                      <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                        <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar" />
-                      </div>
-                      <span className="reviews__user-name">
-                        Max
-                      </span>
-                    </div>
-                    <div className="reviews__info">
-                      <div className="reviews__rating rating">
-                        <div className="reviews__stars rating__stars">
-                          <span style={{width: '80%'}}></span>
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <p className="reviews__text">
-                        A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-                      </p>
-                      <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
-                    </div>
-                  </li>
+                  {
+                    reviews.map((review) => <ReviewsItem review={review} key={review.id}/>)
+                  }
+
                 </ul>
                 <form className="reviews__form form" action="#" method="post">
                   <label className="reviews__label form__label" htmlFor="review">Your review</label>
