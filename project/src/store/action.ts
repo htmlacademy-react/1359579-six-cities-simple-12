d2bfd7d06d0c4d572f6714f266da8ad70401751b
alenushka-jpg <alionxlo@gmail.com>
2023-04-22 13:45:26 +0300

    Описывает новое действие для авторизации

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,6 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
 import { Offers } from '../types/offer';
-import { AuthorizationStatus } from '../const';
+import { AuthorizationStatus, AppRoute } from '../const';
 import { UserData } from '../types/user-data';
 
 const Action = {
@@ -10,6 +10,7 @@ const Action = {
   SET_ERROR: 'SET_ERROR',
   AUTH_REQUIRED: 'AUTH_REQUIRED',
   UPDATE_USER: 'UPDATE_USER',
+  ROUTE_REDIRECTION: 'ROUTE_REDIRECTION',
 };
 
 export const cityChange = createAction<string>(Action.CITY_CHANGE);
@@ -18,3 +19,4 @@ export const setOffersStatus = createAction<boolean>(Action.SET_OFFERS_STATUS);
 export const setError = createAction<string | null>(Action.SET_ERROR);
 export const authRequired = createAction<AuthorizationStatus>(Action.AUTH_REQUIRED);
 export const updateUser = createAction<UserData | null>(Action.UPDATE_USER);
+export const routeRedirection = createAction<AppRoute>(Action.ROUTE_REDIRECTION);
--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -2,9 +2,12 @@ import { AxiosInstance } from 'axios';
 import { AppDispatch, State } from '../types/state';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { Offers } from '../types/offer';
-import { fillOffers, setOffersStatus, setError, authRequired} from './action';
-import { APIRoute, TIMEOUT_SHOW_ERROR, AuthorizationStatus } from '../const';
+import { fillOffers, setOffersStatus, setError, authRequired, updateUser, routeRedirection} from './action';
+import { AppRoute, APIRoute, TIMEOUT_SHOW_ERROR, AuthorizationStatus } from '../const';
 import {store} from './';
+import { AuthData } from '../types/auth-data';
+import { UserData } from '../types/user-data';
+import { saveToken } from '../services/token';
 
 export const clearErrorAction = createAsyncThunk(
   'clearError',
@@ -45,5 +48,22 @@ export const CheckAuthorizationAction = createAsyncThunk<void, undefined, {
     } catch {
       dispatch(authRequired(AuthorizationStatus.NoAuth));
     }
+  },
+);
+
+
+export const loginAction = createAsyncThunk<void, AuthData, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}
+>(
+  'user/login',
+  async({login: email, password}, {dispatch, extra: api}) => {
+    const {data} = await api.post<UserData>(APIRoute.Login, {email,password});
+    saveToken(data.token);
+    dispatch(updateUser(data));
+    dispatch(authRequired(AuthorizationStatus.Auth));
+    dispatch(routeRedirection(AppRoute.Root));
   },
 );
